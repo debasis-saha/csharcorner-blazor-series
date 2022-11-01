2d241dc936dc60563c7f3e2be71d946b2d944b30
DEBASIS SAHA <debasis.ds@outlook.com>
2022-11-01 21:36:26 +0530

    Upload code & presentation PDF related to the Episode 2

---
+++ Episode-02/Demo_on_Razor_Syntax/Helpers/StringUtilities.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace Demo_on_Razor_Syntax.Helpers
+{
+	public class StringUtilityExtensions
+	{
+        public static string ToTitleCase(string value)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            return textInfo.ToTitleCase(value.ToLower());
+        }
+    }
+}
---
+++ Episode-02/Demo_on_Razor_Syntax/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo_on_Razor_Syntax.Models
+{
+    public class Products
+    {
+        public string ProductCode { get; set; }
+
+        public string ProductName { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+
+        public int StockQuantity { get; set; }
+    }
+}
