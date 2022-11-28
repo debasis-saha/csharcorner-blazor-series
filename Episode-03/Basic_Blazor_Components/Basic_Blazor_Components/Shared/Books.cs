e70e450d917ae797cd17ae80511b92dc98af77a1
DEBASIS SAHA <saha.debasis@gmail.com>
2022-11-28 14:31:36 +0530

    Code added for Component with Oneway Bindings...

---
+++ Episode-03/Basic_Blazor_Components/Basic_Blazor_Components/Shared/Books.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Blazor_Components.Shared
+{
+    public class Books
+    {
+        public string BookCode { get; set; }
+
+        public string BookName { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string Category { get; set; }
+
+        public string AboutInfo { get; set; }
+
+        public string CoverImageUrl { get; set; }
+
+        public double Ratings { get; set; }
+    }
+}
